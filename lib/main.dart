188495236ee5555dbe38ef9b1041e61d20bb40cb
Soukorng <vongsoukorng34@gmail.com>
2025-03-26 15:34:11 +0700

    push sign in

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:final_project/signin_screen.dart';
 import 'package:flutter/material.dart';
 import 'browse.dart';
 
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       // home: const Infocontact(contactName: "IW"), // Fix: Pass a contact name
-      home: const browseitem(),
+      home: const SigninScreen(),
     );
   }
 }
